333294f8afd9def233452d3c3962b76a2dec6b1d
jrday-fc <jrday-fc@users.noreply.github.com>
2019-04-09 19:13:22 -0700

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace aspnetapp.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your app description page.";
 
             return View();
         }
